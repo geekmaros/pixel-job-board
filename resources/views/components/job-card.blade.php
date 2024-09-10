0a2f1b93760cf7f94af97d44a56c7f880e794f64
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-10 21:10:39 +0100

    feat: add job card component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracast</div>
+
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Pixel Positions</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div class="">
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="http://placehold.it/42/42" class="rounded-xl" alt="User Avatar">
+    </div>
+</div>
