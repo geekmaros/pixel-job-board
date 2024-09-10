b83a91a67b6d97a3a1eb63a0d9b6ca73cc15259f
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-10 22:51:38 +0100

    refactor: improve job card wide

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,25 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a
+            href=""
+            class="self-start text-sm text-gray-400 transition-colors duration-300"
+        >
+            Laracast
+        </a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">
+            Video Producer
+        </h3>
+        <p class="text-sm font-medium mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div class="">
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-panel>
