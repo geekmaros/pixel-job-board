853806e7fabc5559fea04c3d276b8c4b61d7c5ea
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-10 22:52:05 +0100

    feat: add google font

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,36 +1,50 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pixel Positions</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="bg-black text-white">
-    <div class="px-10">
-        <nav class="flex justify-between items-center py-4 border-b border-white/10">
-            <div>
-                <a href="/">
-                    <img src="{{asset('images/logo.svg')}}" alt="Pixel Positions">
-                </a>
-            </div>
+    <head>
+        <meta charset="UTF-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0"
+        />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Pixel Positions</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:400.500,600&display=swap"
+            rel="stylesheet"
+        />
+        @vite(["resources/css/app.css", "resources/js/app.js"])
+    </head>
+    <body class="bg-black text-white font-sans">
+        <div class="px-10">
+            <nav
+                class="flex justify-between items-center py-4 border-b border-white/10"
+            >
+                <div>
+                    <a href="/">
+                        <img
+                            src="{{ asset("images/logo.svg") }}"
+                            alt="Pixel Positions"
+                        />
+                    </a>
+                </div>
 
-            <div class="space-x-6 font-bold">
-                <a href="#">Jobs</a>
-                <a href="#">Careers</a>
-                <a href="#">Salaries</a>
-                <a href="#">Companies</a>
-            </div>
+                <div class="space-x-6 font-bold">
+                    <a href="#">Jobs</a>
+                    <a href="#">Careers</a>
+                    <a href="#">Salaries</a>
+                    <a href="#">Companies</a>
+                </div>
 
-            <div>
-                <a href="#">Post a Job</a>
-            </div>
-        </nav>
+                <div>
+                    <a href="#">Post a Job</a>
+                </div>
+            </nav>
 
-        <main class="mt-10 max-w[968px] mx-auto">
-            {{ $slot }}
-        </main>
-    </div>
-</body>
+            <main class="mt-10 max-w[968px] mx-auto">
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
 </html>
